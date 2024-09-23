605707666b2d27e861ad0f00f8cab5eed1908751
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-23 13:19:51 +0300

    task20.js

---
+++ task20.js
@@ -0,0 +1,20 @@
+// 1.Task20: Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Input = {"Ryan", "Kieran", "Jason", "Yous"}
+// Output = {"Ryan", "Yous"}
+
+// Input = {"Peter", "Stephen", "Joe"}
+// Output = {}
+// Input strings will only contain letters. Note: keep the original order of the names in the output.
+
+// The solution:
+
+function friend(friends) {
+    return friends.filter((friend) => friend.length === 4);
+}
+
+friend(['Ryan', 'Kieran', 'Mark']); // ["Ryan", "Mark"]
+
+// Time: 819ms Passed: 2Failed: 0
