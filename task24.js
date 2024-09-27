6a7d3fae54794d7d71f558e57a8093030d33d332
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-27 10:47:04 +0300

    added the solution for the task 24 from codeWars

---
+++ task24.js
@@ -0,0 +1,17 @@
+// 1.Task24: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+// The solution:
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+}
+
+DNAtoRNA("TTTT"); // "UUUU"
+
+// Time: 870ms Passed: 4Failed: 0
