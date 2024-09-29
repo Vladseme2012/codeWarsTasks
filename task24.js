d7276cc5640f6c00afcf8cd8b48969a1a848593a
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-09-29 11:57:48 +0300

    correction the solution for task24

--- task24.js
+++ task24.js
@@ -1,15 +1,18 @@
-// 1.Task24: An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// 1.Task24: Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
-// Example: (Input --> Output)
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
 
-// "Dermatoglyphics" --> true
-// "aba" --> false
-// "moOse" --> false (ignore letter case)
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
 // The solution:
 
 function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
+  return dna.replaceAll('T', 'U');
 }
 
 DNAtoRNA("TTTT"); // "UUUU"
