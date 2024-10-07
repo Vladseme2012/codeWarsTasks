e8f05da52a07e2584de7870d10ee66fd3b9bf65b
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-07 13:12:12 +0300

    added the solution for the task 34 from codeWarsadded the solution for the task 33 from codeWars

---
+++ task34.js
@@ -0,0 +1,81 @@
+// 1.Task 34:
+// Instructions
+// Output
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+// Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+// In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
+
+// (
+// a
+// p
+// +
+// b
+// p
+// +
+// 1
+// +
+// c
+// p
+// +
+// 2
+// +
+// d
+// p
+// +
+// 3
+// +
+// .
+// .
+// .
+// )
+// =
+// n
+// ∗
+// k
+// (a
+// p
+//  +b
+// p+1
+//  +c
+// p+2
+//  +d
+// p+3
+//  +...)=n∗k
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be strictly positive integers.
+
+// Examples:
+// n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
+
+// n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
+
+// n = 695; p = 2 ---> 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// n = 46288; p = 3 ---> 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// The solution:
+
+function digPow(n, p) {
+    let sum = 0;
+    let arrNum = n.toString().split('');
+    console.log(arrNum);
+    arrNum.map((el) => {
+        sum += el ** p++;
+    });
+    console.log(sum);
+    if (sum % n) {
+        return -1;
+    } else {
+        return sum / n;
+    }
+}
+
+digPow(89, 1); //1
+
+// Time: 827ms Passed: 1Failed: 0
