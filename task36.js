eb40386071672154d5abecfc670b16c4d98485cd
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-09 11:46:38 +0300

    added the solution for the task 36 from codeWars

---
+++ task36.js
@@ -0,0 +1,17 @@
+// 1.Task 36:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// The solution:
+
+function maps(x){
+  return x.map(el => el * 2)
+
+}
+
+maps([1, 2, 3]); //[2, 4, 6]
+
+// Time: Time: 727ms Passed: 3Failed: 0
