0b6f33333c9d207637390386d5171006c532fdc2
Vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-16 08:50:30 +0300

    added the solution for the task 42 from codeWars

---
+++ task42.js
@@ -0,0 +1,22 @@
+// 1.Task 42:
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+// The solution:
+
+//return the total number of smiling faces in the array
+function paperwork(n, m) {
+    if (n <= 0 || m <= 0) {
+        return 0;
+    }
+    return n * m;
+}
+
+paperwork(5,5); //25;
+
+// Time: 889ms Passed: 2Failed: 0
