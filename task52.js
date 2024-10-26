1e7f0aef5355338649efe015732da9e044e9441b
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-26 11:19:57 +0300

    added the solutin for the task 52 from codeWars

---
+++ task52.js
@@ -0,0 +1,16 @@
+// 1.Task 52:
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// The solution:
+
+function smash(words) {
+    return words.join(' ').trim();
+}
+
+smash(['hello', 'amazing', 'world']); // "hello amazing world"
+
+// Time: 755ms Passed: 4Failed: 0
