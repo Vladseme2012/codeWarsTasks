08eca26af8136c06c7bb87aa2847813bf58b8990
vladseme2012 <vlad.seme2012@yandex.ru>
2024-10-31 19:02:44 +0300

    added the solutin for the task 57 from codeWars

---
+++ task57.js
@@ -0,0 +1,15 @@
+// 1.Task New13:
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+// The solution:
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height;
+    }
+}
+
+Kata.getVolumeOfCuboid(1, 2, 2); // 4
+
+// Time: 737ms Passed: 2Failed: 0
